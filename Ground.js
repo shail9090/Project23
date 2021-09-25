8a6af31eba28fffbe061e3b34b23540ec73eee22
shail9090 <83891379+shail9090@users.noreply.github.com>
2021-09-25 14:03:35 -0700

    Add files via upload

---
+++ Ground.js
@@ -0,0 +1,20 @@
+class Dustbin{
+    constructor(x,y,width,height,color){
+        var options = {
+
+            isStatic: false
+        }
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.width = width;
+        this.height = height;
+        this.color = color;
+        World.add(world, this.body);
+    }
+    display(){
+        var pos =this.body.position;
+        rectMode(CENTER);
+        fill(this.color);
+        rect(pos.x, pos.y, this.width, this.height);
+      }
+    };
+
